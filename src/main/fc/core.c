b3b6046d9908f98abcf2eb5abb2161135ac2334f
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-05-20 11:00:04 +0100

    RTFL: Remove MAG_MODE

--- src/main/fc/core.c
+++ src/main/fc/core.c
@@ -485,24 +485,6 @@ static void updateInflightCalibrationState(void)
     }
 }
 
-#if defined(USE_GPS) || defined(USE_MAG)
-static void updateMagHold(void)
-{
-    if (fabsf(rcCommand[YAW]) < 15 && FLIGHT_MODE(MAG_MODE)) {
-        int16_t dif = DECIDEGREES_TO_DEGREES(attitude.values.yaw) - magHold;
-        if (dif <= -180)
-            dif += 360;
-        if (dif >= +180)
-            dif -= 360;
-        dif *= -GET_DIRECTION(rcControlsConfig()->yaw_control_reversed);
-        if (isUpright()) {
-            rcCommand[YAW] -= dif * currentPidProfile->pid[PID_MAG].P / 30;    // 18 deg
-        }
-    } else
-        magHold = DECIDEGREES_TO_DEGREES(attitude.values.yaw);
-}
-#endif
-
 #ifdef USE_VTX_CONTROL
 static bool canUpdateVTX(void)
 {
@@ -687,21 +669,6 @@ void processRxModes(timeUs_t currentTimeUs)
         DISABLE_ARMING_FLAG(WAS_ARMED_WITH_PREARM);
     }
 
-#if defined(USE_ACC) || defined(USE_MAG)
-    if (sensors(SENSOR_ACC) || sensors(SENSOR_MAG)) {
-#if defined(USE_GPS) || defined(USE_MAG)
-        if (IS_RC_MODE_ACTIVE(BOXMAG)) {
-            if (!FLIGHT_MODE(MAG_MODE)) {
-                ENABLE_FLIGHT_MODE(MAG_MODE);
-                magHold = DECIDEGREES_TO_DEGREES(attitude.values.yaw);
-            }
-        } else {
-            DISABLE_FLIGHT_MODE(MAG_MODE);
-        }
-#endif
-    }
-#endif
-
     if (IS_RC_MODE_ACTIVE(BOXPASSTHRU)) {
         ENABLE_FLIGHT_MODE(PASSTHRU_MODE);
     } else {
@@ -760,12 +727,6 @@ static FAST_CODE_NOINLINE void subTaskPidSubprocesses(timeUs_t currentTimeUs)
     if (debugMode == DEBUG_PIDLOOP) {
         startTime = micros();
     }
-
-#if defined(USE_GPS) || defined(USE_MAG)
-    if (sensors(SENSOR_GPS) || sensors(SENSOR_MAG)) {
-        updateMagHold();
-    }
-#endif
 
 #ifdef USE_BLACKBOX
     if (!cliMode && blackboxConfig()->device) {
