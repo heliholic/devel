ab6d8ea21d290c24ceb9be2a4a4de4bc369c0ef8
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-05-25 12:13:15 +0100

    RTFL: Refactor mixer interface

--- src/main/config/config.c
+++ src/main/config/config.c
@@ -769,7 +769,6 @@ void changePidProfile(uint8_t pidProfileIndex)
         loadPidProfile();
 
         pidInit(currentPidProfile);
-        mixerInitProfile();
     }
 
     beeperConfirmationBeeps(pidProfileIndex + 1);
