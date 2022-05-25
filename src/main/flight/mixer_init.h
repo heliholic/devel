7cd8ec024a3fea11ae99e9e26aec0a656508941a
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-05-25 01:53:54 +0100

    RTFL: Refactor motor interface

--- src/main/flight/mixer_init.h
+++ src/main/flight/mixer_init.h
@@ -24,27 +24,6 @@
 
 #include "flight/mixer.h"
 
+static inline void mixerInit(void) {}
+static inline void mixerInitProfile(void) {}
 
-typedef struct mixerRuntime_s {
-    uint8_t motorCount;
-    motorMixer_t currentMixer[MAX_SUPPORTED_MOTORS];
-    bool feature3dEnabled;
-    float motorOutputLow;
-    float motorOutputHigh;
-    float disarmMotorOutput;
-    float deadbandMotor3dHigh;
-    float deadbandMotor3dLow;
-#ifdef USE_DYN_IDLE
-    float dynIdleMaxIncrease;
-    float idleThrottleOffset;
-    float dynIdleMinRps;
-    float dynIdlePGain;
-    float prevMinRps;
-    float dynIdleIGain;
-    float dynIdleDGain;
-    float dynIdleI;
-    float minRpsDelayK;
-#endif
-} mixerRuntime_t;
-
-extern mixerRuntime_t mixerRuntime;
