384e9e883ee0bc84c6f76aa6e33f2e6790bb166f
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-06-10 16:09:20 +0100

    RTFL: Move feedforward filter to rc.c

--- src/main/flight/pid.h
+++ src/main/flight/pid.h
@@ -167,13 +167,6 @@ typedef struct pidRuntime_s {
     bool itermRotation;
     bool zeroThrottleItermReset;
 
-#ifdef USE_RC_SMOOTHING_FILTER
-    pt3Filter_t feedforwardPt3[XYZ_AXIS_COUNT];
-    bool feedforwardLpfInitialized;
-    uint8_t rcSmoothingDebugAxis;
-    uint8_t rcSmoothingFilterType;
-#endif // USE_RC_SMOOTHING_FILTER
-
 #ifdef USE_DYN_LPF
     uint8_t dynLpfFilter;
     uint16_t dynLpfMin;
