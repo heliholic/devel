f994d607d851d478fc2629aa0abbd312ff2a7a37
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-07-11 23:51:59 +0100

    RTFL: Simplify rc_smoothing phase 1

--- src/main/fc/rc_smoothing.c
+++ src/main/fc/rc_smoothing.c
@@ -63,7 +63,6 @@
 #define RC_SMOOTHING_FILTER_TRAINING_DELAY_MS   1000  // Additional time to wait after receiving first valid rx frame before initial training starts
 #define RC_SMOOTHING_FILTER_RETRAINING_DELAY_MS 2000  // Guard time to wait after retraining to prevent retraining again too quickly
 #define RC_SMOOTHING_RX_RATE_CHANGE_PERCENT     20    // Look for samples varying this much from the current detected frame rate to initiate retraining
-#define RC_SMOOTHING_SP_DELTA_INITIAL_HZ        100   // The value to use for "auto"
 
 
 static FAST_DATA_ZERO_INIT rcSmoothingFilter_t rcSmoothing;
@@ -72,7 +71,7 @@ static FAST_DATA_ZERO_INIT rcSmoothingFilter_t rcSmoothing;
 static void rcSmoothingResetAccumulation(void)
 {
     rcSmoothing.training.sum = 0;
-    rcSmoothing.training.count = 0;
+    rcSmoothing.training.cnt = 0;
     rcSmoothing.training.min = UINT16_MAX;
     rcSmoothing.training.max = 0;
 }
@@ -80,16 +79,16 @@ static void rcSmoothingResetAccumulation(void)
 static FAST_CODE bool rcSmoothingAccumulateSample(int rxFrameTimeUs)
 {
     rcSmoothing.training.sum += rxFrameTimeUs;
-    rcSmoothing.training.count++;
+    rcSmoothing.training.cnt++;
     rcSmoothing.training.max = MAX(rcSmoothing.training.max, rxFrameTimeUs);
     rcSmoothing.training.min = MIN(rcSmoothing.training.min, rxFrameTimeUs);
 
     // if we've collected enough samples then calculate the average and reset the accumulation
     const int sampleLimit = (rcSmoothing.filterInitialized) ? RC_SMOOTHING_FILTER_RETRAINING_SAMPLES : RC_SMOOTHING_FILTER_TRAINING_SAMPLES;
 
-    if (rcSmoothing.training.count >= sampleLimit) {
-        rcSmoothing.training.sum = rcSmoothing.training.sum - rcSmoothing.training.min - rcSmoothing.training.max; // Throw out high and low samples
-        rcSmoothing.averageFrameTimeUs = lrintf(rcSmoothing.training.sum / (rcSmoothing.training.count - 2));
+    if (rcSmoothing.training.cnt >= sampleLimit) {
+        rcSmoothing.training.sum -= rcSmoothing.training.min + rcSmoothing.training.max; // Throw out high and low samples
+        rcSmoothing.averageFrameTimeUs = rcSmoothing.training.sum / (rcSmoothing.training.cnt - 2);
         rcSmoothingResetAccumulation();
         return true;
     }
@@ -99,14 +98,15 @@ static FAST_CODE bool rcSmoothingAccumulateSample(int rxFrameTimeUs)
 
 static inline int calcAutoSmoothingCutoff(int avgRxFrameTimeUs, uint8_t autoSmoothnessFactor)
 {
-    float cutoff = 0;
+    int cutoff = 0;
 
     if (avgRxFrameTimeUs > 0) {
-        const float cutoffFactor = 1.5f / (1.0f + (autoSmoothnessFactor / 10.0f));
-        cutoff = (1e6f / avgRxFrameTimeUs) * cutoffFactor;
+        // factor = 1.5f / (1.0f + (autoSmoothnessFactor / 10.0f));
+        // cutoff = factor * rxFrameRate = factor * (1.0f / (avgRxFrameTimeUs * 1e-6f));
+        cutoff = 15000000 / (avgRxFrameTimeUs * (10 + autoSmoothnessFactor));
     }
 
-    return MAX(RC_SMOOTHING_CUTOFF_MIN_HZ, lrintf(cutoff));
+    return MAX(RC_SMOOTHING_CUTOFF_MIN_HZ, cutoff);
 }
 
 
@@ -115,42 +115,21 @@ static FAST_CODE void rcSmoothingSetFilterCutoffs(void)
     const float dT = pidGetDT();
     uint16_t cutoff;
 
-    if (rcSmoothing.setpointCutoffSetting == 0) {
-        cutoff = calcAutoSmoothingCutoff(rcSmoothing.averageFrameTimeUs, rcSmoothing.autoSmoothnessFactorSetpoint);
-        if (rcSmoothing.setpointCutoffFrequency != cutoff) {
-            float gain = pt3FilterGain(rcSmoothing.setpointCutoffFrequency, dT);
+    if (rxConfig()->rc_smoothing_cutoff == 0) {
+        cutoff = calcAutoSmoothingCutoff(rcSmoothing.averageFrameTimeUs, rxConfig()->rc_smoothing_factor);
+        if (rcSmoothing.cutoffFreq != cutoff) {
+            float gain = pt3FilterGain(cutoff, dT);
             for (int i = 0; i < 4; i++)
-                pt3FilterUpdateCutoff(&rcSmoothing.setpointFilter[i], gain);
-            rcSmoothing.setpointCutoffFrequency = cutoff;
-        }
-    }
-
-    if (rcSmoothing.throttleCutoffSetting == 0) {
-        cutoff = calcAutoSmoothingCutoff(rcSmoothing.averageFrameTimeUs, rcSmoothing.autoSmoothnessFactorThrottle);
-        if (rcSmoothing.throttleCutoffFrequency != cutoff) {
-            float gain = pt3FilterGain(rcSmoothing.throttleCutoffFrequency, dT);
-            pt3FilterUpdateCutoff(&rcSmoothing.setpointFilter[4], gain);
-            rcSmoothing.throttleCutoffFrequency = cutoff;
-        }
-    }
-
-    if (rcSmoothing.setpointDeltaCutoffSetting == 0) {
-        cutoff = calcAutoSmoothingCutoff(rcSmoothing.averageFrameTimeUs, rcSmoothing.autoSmoothnessFactorSetpoint);
-        if (rcSmoothing.setpointDeltaCutoffFrequency != cutoff) {
-            float gain = pt3FilterGain(rcSmoothing.setpointDeltaCutoffFrequency, dT);
-            for (int i = 0; i < 3; i++)
-                pt3FilterUpdateCutoff(&rcSmoothing.setpointDeltaFilter[i], gain);
-            rcSmoothing.setpointDeltaCutoffFrequency = cutoff;
+                pt3FilterUpdateCutoff(&rcSmoothing.filter[i], gain);
+            rcSmoothing.cutoffFreq = cutoff;
         }
     }
 }
 
 
-FAST_CODE bool rcSmoothingAutoCalculate(void)
+bool rcSmoothingAutoCalculate(void)
 {
-    return ((rcSmoothing.setpointCutoffSetting == 0) ||
-            (rcSmoothing.setpointDeltaCutoffSetting == 0) ||
-            (rcSmoothing.throttleCutoffSetting == 0));
+    return (rxConfig()->rc_smoothing_cutoff == 0);
 }
 
 
@@ -163,18 +142,8 @@ INIT_CODE void rcSmoothingFilterInit(void)
 
         rcSmoothing.averageFrameTimeUs = 0;
 
-        rcSmoothing.autoSmoothnessFactorSetpoint = rxConfig()->rc_smoothing_auto_factor_rpy;
-        rcSmoothing.autoSmoothnessFactorThrottle = rxConfig()->rc_smoothing_auto_factor_throttle;
-        rcSmoothing.setpointCutoffSetting = rxConfig()->rc_smoothing_setpoint_cutoff;
-        rcSmoothing.throttleCutoffSetting = rxConfig()->rc_smoothing_throttle_cutoff;
-        rcSmoothing.setpointDeltaCutoffSetting = rxConfig()->rc_smoothing_setpoint_delta_cutoff;
-
-        rcSmoothing.setpointCutoffFrequency = (rcSmoothing.setpointCutoffSetting) ?
-            rcSmoothing.setpointCutoffSetting : RC_SMOOTHING_CUTOFF_MIN_HZ;
-        rcSmoothing.throttleCutoffFrequency = (rcSmoothing.throttleCutoffSetting) ?
-            rcSmoothing.throttleCutoffSetting : RC_SMOOTHING_CUTOFF_MIN_HZ;
-        rcSmoothing.setpointDeltaCutoffFrequency = (rcSmoothing.setpointDeltaCutoffSetting) ?
-            rcSmoothing.setpointDeltaCutoffSetting : RC_SMOOTHING_SP_DELTA_INITIAL_HZ;
+        rcSmoothing.cutoffFreq = (rxConfig()->rc_smoothing_cutoff) ?
+            rxConfig()->rc_smoothing_cutoff : RC_SMOOTHING_CUTOFF_MIN_HZ;
 
         rcSmoothing.debugAxis = rxConfig()->rc_smoothing_debug_axis;
 
@@ -182,13 +151,9 @@ INIT_CODE void rcSmoothingFilterInit(void)
 
         const float dT = pidGetDT();
 
-        for (int i = 0; i < 4; i++)
-            pt3FilterInit(&rcSmoothing.setpointFilter[i], pt3FilterGain(rcSmoothing.setpointCutoffFrequency, dT));
-
-        for (int i = 0; i < 3; i++)
-            pt3FilterInit(&rcSmoothing.setpointDeltaFilter[i], pt3FilterGain(rcSmoothing.setpointDeltaCutoffFrequency, dT));
-
-        pt3FilterInit(&rcSmoothing.setpointFilter[4], pt3FilterGain(rcSmoothing.throttleCutoffFrequency, dT));
+        for (int i = 0; i < 4; i++) {
+            pt3FilterInit(&rcSmoothing.filter[i], pt3FilterGain(rcSmoothing.cutoffFreq, dT));
+        }
 
         rcSmoothing.calculateCutoffs = rcSmoothingAutoCalculate();
 
@@ -226,7 +191,7 @@ FAST_CODE void rcSmoothingFilterUpdate(bool isRxRateValid, uint16_t currentRxRef
                     // During initial training process all samples.
                     // During retraining check samples to determine if they vary by more than the limit percentage.
                     if (rcSmoothing.filterInitialized) {
-                        const float percentChange = (ABS(currentRxRefreshRate - rcSmoothing.averageFrameTimeUs) / (float)rcSmoothing.averageFrameTimeUs) * 100;
+                        const int percentChange = (100 * ABS((int)currentRxRefreshRate - (int)rcSmoothing.averageFrameTimeUs)) / rcSmoothing.averageFrameTimeUs;
                         if (percentChange < RC_SMOOTHING_RX_RATE_CHANGE_PERCENT) {
                             // We received a sample that wasn't more than the limit percent so reset the accumulation
                             // During retraining we need a contiguous block of samples that are all significantly different than the current average
@@ -253,35 +218,27 @@ FAST_CODE void rcSmoothingFilterUpdate(bool isRxRateValid, uint16_t currentRxRef
 
         // rx frame rate training blackbox debugging
         DEBUG_SET(DEBUG_RC_SMOOTHING_RATE, 0, currentRxRefreshRate);              // log each rx frame interval
-        DEBUG_SET(DEBUG_RC_SMOOTHING_RATE, 1, rcSmoothing.training.count);        // log the training step count
+        DEBUG_SET(DEBUG_RC_SMOOTHING_RATE, 1, rcSmoothing.training.cnt);          // log the training step cnt
         DEBUG_SET(DEBUG_RC_SMOOTHING_RATE, 2, rcSmoothing.averageFrameTimeUs);    // the current calculated average
         DEBUG_SET(DEBUG_RC_SMOOTHING_RATE, 3, sampleState);                       // indicates whether guard time is active
     }
 }
 
-FAST_CODE float rcSmoothingFilterApply(int axis, float rawSetpoint)
+FAST_CODE float rcSmoothingFilterApply(int axis, float input)
 {
-    float newSetpoint = pt3FilterApply(&rcSmoothing.setpointFilter[axis], rawSetpoint);
+    float output = input;
 
-    if (axis == rcSmoothing.debugAxis) {
-        DEBUG_SET(DEBUG_RC_SMOOTHING, 1, lrintf(rawSetpoint * 100.0f));
-        DEBUG_SET(DEBUG_RC_SMOOTHING, 2, lrintf(newSetpoint * 100.0f));
+    // RPYC primary controls only
+    if (axis < 4) {
+        output = pt3FilterApply(&rcSmoothing.filter[axis], input);
+
+        if (axis == rcSmoothing.debugAxis) {
+            DEBUG_SET(DEBUG_RC_SMOOTHING, 1, lrintf(input * 1000));
+            DEBUG_SET(DEBUG_RC_SMOOTHING, 2, lrintf(output * 1000));
+        }
     }
 
-    return newSetpoint;
-}
-
-
-FAST_CODE float rcSmoothingDeltaFilterApply(int axis, float rawDelta)
-{
-    float newDelta = pt3FilterApply(&rcSmoothing.setpointDeltaFilter[axis], rawDelta);
-
-    if (axis == rcSmoothing.debugAxis) {
-        DEBUG_SET(DEBUG_RC_SMOOTHING, 3, lrintf(rawDelta * 100.0f));
-        DEBUG_SET(DEBUG_RC_SMOOTHING, 4, lrintf(newDelta * 100.0f));
-    }
-
-    return newDelta;
+    return output;
 }
 
 
