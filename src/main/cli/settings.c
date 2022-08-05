db6747db949af8407a470c604ae9ff4b2edd4681
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-08-05 14:21:32 +0100

    WIP: Add esc_sensor_update_hz

--- src/main/cli/settings.c
+++ src/main/cli/settings.c
@@ -1361,6 +1361,7 @@ const clivalue_t valueTable[] = {
 #ifdef USE_ESC_SENSOR
     { "esc_sensor_halfduplex",          VAR_UINT8   | MASTER_VALUE | MODE_LOOKUP, .config.lookup = { TABLE_OFF_ON }, PG_ESC_SENSOR_CONFIG, offsetof(escSensorConfig_t, halfDuplex) },
     { "esc_sensor_current_offset",      VAR_UINT16  | MASTER_VALUE, .config.minmaxUnsigned = { 0, 16000 }, PG_ESC_SENSOR_CONFIG, offsetof(escSensorConfig_t, offset) },
+    { "esc_sensor_update_hz",           VAR_UINT16  | MASTER_VALUE, .config.minmaxUnsigned = { 10, 500 }, PG_ESC_SENSOR_CONFIG, offsetof(escSensorConfig_t, update_hz) },
 #endif
 
 #ifdef USE_RX_FRSKY_SPI
