e22285f6948c7a1495aaade181e0b10678b36a08
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-08-05 14:21:32 +0100

    WIP: Add esc_sensor_update_hz

--- src/main/sensors/esc_sensor.h
+++ src/main/sensors/esc_sensor.h
@@ -31,7 +31,7 @@
 typedef struct escSensorConfig_s {
     uint8_t halfDuplex;             // Set to false to listen on the TX pin for telemetry data
     uint16_t offset;                // offset consumed by the flight controller / VTX / cam / ... in milliampere
-
+    uint16_t update_hz;             // Update frequency
 } escSensorConfig_t;
 
 PG_DECLARE(escSensorConfig_t, escSensorConfig);
