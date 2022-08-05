967eee5972e36d33ae8aad077cbd265a410dc9d8
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-08-05 12:45:41 +0100

    RTFL: Clean up PID controller

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -86,7 +86,6 @@
 #include "flight/imu.h"
 #include "flight/mixer.h"
 #include "flight/pid.h"
-#include "flight/pid_init.h"
 #include "flight/position.h"
 #include "flight/rpm_filter.h"
 #include "flight/servos.h"
@@ -1300,13 +1299,13 @@ static bool mspProcessOutCommand(int16_t cmdMSP, sbuf_t *dst)
         break;
 
     case MSP_PIDNAMES:
-        for (const char *c = pidNames; *c; c++) {
+        for (const char *c = PID_NAMES; *c; c++) {
             sbufWriteU8(dst, *c);
         }
         break;
 
     case MSP_PID_CONTROLLER:
-        sbufWriteU8(dst, PID_CONTROLLER_BETAFLIGHT);
+        sbufWriteU8(dst, PID_CONTROLLER_ROTORFLIGHT);
         break;
 
     case MSP_MODE_RANGES:
@@ -1802,9 +1801,9 @@ static bool mspProcessOutCommand(int16_t cmdMSP, sbuf_t *dst)
         sbufWriteU8(dst, 0); // reserved
         sbufWriteU8(dst, 0); // reserved
         sbufWriteU8(dst, 0); // reserved
-        sbufWriteU16(dst, currentPidProfile->rateAccelLimit);
-        sbufWriteU16(dst, currentPidProfile->yawRateAccelLimit);
-        sbufWriteU8(dst, currentPidProfile->levelAngleLimit);
+        sbufWriteU16(dst, 0); // was currentPidProfile->rateAccelLimit
+        sbufWriteU16(dst, 0); // was currentPidProfile->yawRateAccelLimit
+        sbufWriteU8(dst, 0); // was currentPidProfile->levelAngleLimit
         sbufWriteU8(dst, 0); // was pidProfile.levelSensitivity
         sbufWriteU16(dst, 0); // was currentPidProfile->itermThrottleThreshold
         sbufWriteU16(dst, 0); // was currentPidProfile->itermAcceleratorGain
@@ -1815,11 +1814,7 @@ static bool mspProcessOutCommand(int16_t cmdMSP, sbuf_t *dst)
         sbufWriteU8(dst, 0); // was urrentPidProfile->iterm_relax_type
         sbufWriteU8(dst, 0); // was currentPidProfile->abs_control_gain
         sbufWriteU8(dst, 0);
-#if defined(USE_ACRO_TRAINER)
-        sbufWriteU8(dst, currentPidProfile->acro_trainer_angle_limit);
-#else
-        sbufWriteU8(dst, 0);
-#endif
+        sbufWriteU8(dst, 0); // was currentPidProfile->acro_trainer_angle_limit
         sbufWriteU16(dst, currentPidProfile->pid[PID_ROLL].F);
         sbufWriteU16(dst, currentPidProfile->pid[PID_PITCH].F);
         sbufWriteU16(dst, currentPidProfile->pid[PID_YAW].F);
@@ -2215,7 +2210,7 @@ static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t cmdMSP, 
             currentPidProfile->pid[i].I = sbufReadU8(src);
             currentPidProfile->pid[i].D = sbufReadU8(src);
         }
-        pidInitConfig(currentPidProfile);
+        pidInitProfile(currentPidProfile);
         break;
 
     case MSP_SET_MODE_RANGE:
@@ -2594,10 +2589,10 @@ static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t cmdMSP, 
         sbufReadU8(src); // reserved
         sbufReadU8(src); // reserved
         sbufReadU8(src); // reserved
-        currentPidProfile->rateAccelLimit = sbufReadU16(src);
-        currentPidProfile->yawRateAccelLimit = sbufReadU16(src);
+        sbufReadU16(src); // was currentPidProfile->rate_accel_limit
+        sbufReadU16(src); // was currentPidProfile->yaw_rate_accel_limit
         if (sbufBytesRemaining(src) >= 2) {
-            currentPidProfile->levelAngleLimit = sbufReadU8(src);
+            sbufReadU8(src); // was currentPidProfile->levelAngleLimit
             sbufReadU8(src); // was pidProfile.levelSensitivity
         }
         if (sbufBytesRemaining(src) >= 4) {
@@ -2615,11 +2610,7 @@ static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t cmdMSP, 
             sbufReadU8(src); // was currentPidProfile->iterm_relax_type
             sbufReadU8(src); // was currentPidProfile->abs_control_gain
             sbufReadU8(src);
-#if defined(USE_ACRO_TRAINER)
-            currentPidProfile->acro_trainer_angle_limit = sbufReadU8(src);
-#else
-            sbufReadU8(src);
-#endif
+            sbufReadU8(src); // currentPidProfile->was acro_trainer_angle_limit
             // PID controller feedforward terms
             currentPidProfile->pid[PID_ROLL].F = sbufReadU16(src);
             currentPidProfile->pid[PID_PITCH].F = sbufReadU16(src);
@@ -2657,7 +2648,7 @@ static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t cmdMSP, 
             sbufReadU8(src);
             sbufReadU8(src);
         }
-        pidInitConfig(currentPidProfile);
+        pidInitProfile(currentPidProfile);
 
         break;
     case MSP_SET_SENSOR_CONFIG:
