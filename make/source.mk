ed3ad7ee99f0b927cd3ecd08326071c64fb91028
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-08-25 13:09:15 +0100

    WIP: Add AGC first bits and bobs

--- make/source.mk
+++ make/source.mk
@@ -94,6 +94,7 @@ COMMON_SRC = \
             flight/mixer.c \
             flight/mixer_init.c \
             flight/pid.c \
+            flight/agc.c \
             flight/rpm_filter.c \
             flight/motors.c \
             flight/servos.c \
