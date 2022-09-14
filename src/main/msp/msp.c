396723096d3c00f4a1fd062c2562fdecf26490e7
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-09-14 10:34:56 +0100

    WIP: Add adjustment ranges

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -1332,6 +1332,7 @@ static bool mspProcessOutCommand(int16_t cmdMSP, sbuf_t *dst)
         }
         break;
 
+#if 0
     case MSP_ADJUSTMENT_RANGES:
         for (int i = 0; i < MAX_ADJUSTMENT_RANGE_COUNT; i++) {
             const adjustmentRange_t *adjRange = adjustmentRanges(i);
@@ -1343,6 +1344,7 @@ static bool mspProcessOutCommand(int16_t cmdMSP, sbuf_t *dst)
             sbufWriteU8(dst, adjRange->auxSwitchChannelIndex);
         }
         break;
+#endif
 
     case MSP_MOTOR_CONFIG:
         sbufWriteU16(dst, motorConfig()->minthrottle);
@@ -2239,6 +2241,7 @@ static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t cmdMSP, 
         }
         break;
 
+#if 0
     case MSP_SET_ADJUSTMENT_RANGE:
         i = sbufReadU8(src);
         if (i < MAX_ADJUSTMENT_RANGE_COUNT) {
@@ -2255,6 +2258,7 @@ static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t cmdMSP, 
             return MSP_RESULT_ERROR;
         }
         break;
+#endif
 
     case MSP_SET_RC_TUNING:
         if (sbufBytesRemaining(src) >= 10) {
