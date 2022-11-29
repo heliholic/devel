fa01e80bca0ac9748a05f32ee6beff0b84acd3ce
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-11-29 14:58:53 +0000

    WIP: Refactor servo configuration

--- src/main/config/config.c
+++ src/main/config/config.c
@@ -222,7 +222,7 @@ static void validateAndFixServoConfig()
 {
     for (int i = 0; i < MAX_SUPPORTED_SERVOS; i++) {
 #ifndef USE_SERVO_GEOMETRY_CORRECTION
-        servoParamsMutable(i)->flags &= ~SERVO_FLAG_GEOMETRY_CORRECTION;
+        servoParamsMutable(i)->flags &= ~SERVO_FLAG_GEO_CORR;
 #endif
     }
 }
@@ -247,8 +247,8 @@ static void validateAndFixConfig(void)
         featureDisableImmediate(FEATURE_GPS);
     }
 
-    if ((motorConfig()->dev.motorPwmProtocol == PWM_TYPE_STANDARD) && (motorConfig()->dev.motorPwmRate > BRUSHLESS_MOTORS_PWM_RATE)) {
-        motorConfigMutable()->dev.motorPwmRate = BRUSHLESS_MOTORS_PWM_RATE;
+    if ((motorConfig()->dev.motorPwmProtocol == PWM_TYPE_STANDARD) && (motorConfig()->dev.motorPwmRate > MOTORS_MAX_PWM_RATE)) {
+        motorConfigMutable()->dev.motorPwmRate = MOTORS_MAX_PWM_RATE;
     }
 
     validateAndFixGyroConfig();
@@ -601,7 +601,7 @@ void validateAndFixGyroConfig(void)
 
         switch (motorConfig()->dev.motorPwmProtocol) {
         case PWM_TYPE_STANDARD:
-                motorUpdateRestriction = 1.0f / BRUSHLESS_MOTORS_PWM_RATE;
+                motorUpdateRestriction = 1.0f / MOTORS_MAX_PWM_RATE;
                 break;
         case PWM_TYPE_ONESHOT125:
                 motorUpdateRestriction = 0.0005f;
