462ea940a1c4348ffdff0aa548a37afefec3a8e5
Petri Mattila <petri.t.j.mattila@gmail.com>
2023-01-09 11:59:36 +0000

    Use measured filter task speed in rpm_filter
    The gyro may be running sligtly off the configured frequency,
    and thus the filter task speed may not be exactly as expected.
    
    Let's use the measured filter task execution speed for setting
    up the RPM filters, as they are very sensitive to any frequency error.

--- src/main/flight/rpm_filter.c
+++ src/main/flight/rpm_filter.c
@@ -173,6 +173,9 @@ void rpmFilterUpdate()
 {
     if (activeBankCount > 0) {
 
+        // Use the measured filter task speed
+        uint32_t looptime = getTaskAverateCycleTime(TASK_FILTER);
+
         // Update one filter bank per cycle
         rpmFilterBank_t *filt = &filterBank[currentBank];
 
@@ -186,7 +189,7 @@ void rpmFilterUpdate()
         biquadFilter_t *Y = &filt->notch[2];
 
         // Update the filter coefficients
-        biquadFilterUpdate(R, freq, gyro.filterLooptime, filt->Q, FILTER_NOTCH, 1);
+        biquadFilterUpdate(R, freq, looptime, filt->Q, FILTER_NOTCH, 1);
 
         // Transfer the filter coefficients from Roll axis filter into Pitch and Yaw
         P->b0 = Y->b0 = R->b0;
