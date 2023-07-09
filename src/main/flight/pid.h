5dcc43a79390a58fced9a1c62d83fd031af730a1
Petri Mattila <petri.t.j.mattila@gmail.com>
2023-07-09 22:36:59 +0100

    Add PID Mode 3 with Fast Flight I-term and linear decay

--- src/main/flight/pid.h
+++ src/main/flight/pid.h
@@ -57,10 +57,12 @@ typedef struct {
     float I;
     float D;
     float F;
+    float O;
     float pidSum;
     float setPoint;
     float gyroRate;
     float axisError;
+    float axisOffset;
 } pidAxisData_t;
 
 typedef struct {
@@ -68,6 +70,7 @@ typedef struct {
     float Ki;
     float Kd;
     float Kf;
+    float Ko;
 } pidAxisCoef_t;
 
 typedef struct {
@@ -99,9 +102,14 @@ typedef struct pid_s {
 
     uint8_t errorRotation;
 
-    float errorDecayYaw;
-    float errorDecayCyclic;
-    float errorDecayGround;
+    float errorDecayRateGround;
+
+    float errorDecayRateCyclic;
+    float errorDecayLimitCyclic;
+    float errorDecayRateOffset;
+    float errorDecayLimitOffset;
+    float errorDecayRateYaw;
+    float errorDecayLimitYaw;
 
     float errorLimit[PID_AXIS_COUNT];
 
