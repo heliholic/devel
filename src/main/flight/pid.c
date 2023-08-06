c1d43ef3801b69bcf9b08293185c8a1a59451617
Petri Mattila <petri.t.j.mattila@gmail.com>
2023-08-06 11:27:09 +0100

    Add cyclic crosstalk precomp (pitch-to-roll)

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -210,6 +210,13 @@ void INIT_CODE pidInitProfile(const pidProfile_t *pidProfile)
     // Pitch precomp
     pid.precomp.pitchCollectiveFFGain = pidProfile->pitch_collective_ff_gain / 500.0f;
 
+    // Pitch-to-Roll derivative feedback
+    pid.cyclicCrosstalkMode = pidProfile->cyclic_crosstalk_mode;
+    pid.cyclicCrosstalkGain = pidProfile->cyclic_crosstalk_gain * mixerRotationSign() * -CYCLIC_CROSSTALK_SCALE;
+
+    // Pitch derivative filter
+    difFilterInit(&pid.crossTalkFilter, pidProfile->cyclic_crosstalk_cutoff, pid.freq);
+
     // Initialise sub-profiles
     governorInitProfile(pidProfile);
 #ifdef USE_ACC
@@ -399,6 +406,21 @@ static void pidApplyPrecomp(void)
     DEBUG(PITCH_PRECOMP, 0, collectiveDeflection * 1000);
     DEBUG(PITCH_PRECOMP, 1, pitchPrecomp * 1000);
 }
+
+static void pidApplyCyclicCrosstalk(void)
+{
+    // Derivative filter
+    const float pitchRate = pid.cyclicCrosstalkMode ? pid.data[FD_PITCH].setPoint : pid.data[FD_PITCH].gyroRate;
+    const float pitchDeriv = difFilterApply(&pid.crossTalkFilter, pitchRate);
+    const float rollComp = pitchDeriv * pid.cyclicCrosstalkGain;
+
+    // Add to ROLL
+    pid.data[FD_ROLL].pidSum += rollComp;
+
+    DEBUG(PITCH_PRECOMP, 2, pitchDeriv);
+    DEBUG(PITCH_PRECOMP, 3, rollComp * 1000);
+}
+
 
 /** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **
  **
@@ -915,11 +937,13 @@ void pidController(const pidProfile_t *pidProfile, timeUs_t currentTimeUs)
         case 9:
             pidApplyCyclicMode9(PID_ROLL);
             pidApplyCyclicMode9(PID_PITCH);
+            pidApplyCyclicCrosstalk();
             pidApplyYawMode9();
             break;
         case 2:
             pidApplyCyclicMode2(PID_ROLL);
             pidApplyCyclicMode2(PID_PITCH);
+            pidApplyCyclicCrosstalk();
             pidApplyYawMode2();
             break;
         case 1:
