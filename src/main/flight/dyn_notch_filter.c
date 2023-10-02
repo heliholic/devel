7e6d74f6870680e6a56f7ecc81482878479c5b7c
Petri Mattila <petri.t.j.mattila@gmail.com>
2023-10-02 11:27:51 +0100

    Refactor RX configuration

--- src/main/flight/dyn_notch_filter.c
+++ src/main/flight/dyn_notch_filter.c
@@ -48,6 +48,7 @@
 #include "drivers/time.h"
 
 #include "fc/core.h"
+#include "fc/rc.h"
 
 #include "sensors/gyro.h"
 
@@ -326,7 +327,7 @@ static FAST_CODE_NOINLINE void dynNotchProcess(void)
                 }
             }
 
-            if(calculateThrottlePercentAbs() > DYN_NOTCH_OSD_MIN_THROTTLE) {
+            if (getThrottlePercent() > DYN_NOTCH_OSD_MIN_THROTTLE) {
                 for (int p = 0; p < dynNotch.count; p++) {
                     dynNotch.maxCenterFreq = MAX(dynNotch.maxCenterFreq, dynNotch.centerFreq[state.axis][p]);
                 }
