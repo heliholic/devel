322ce0e57a3a1de1beb202e560e51cd98b3135ce
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-03-11 17:59:32 +0000

    Extract script

---
+++ src/utils/extract-release-notes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+
+def main(args):
+    release = args[1]
+    inputfile = args[2]
+    outputfile = args[3]
+
+    found = False
+
+    with open(inputfile) as input:
+        with open(outputfile, 'w') as output:
+            for line in input:
+                if line.startswith('# '):
+                    found = line.strip() == '# ' + release
+                elif line.startswith('***'):
+                    found = False
+                if found:
+                    output.write(line)
+
+
+if __name__ == '__main__': main(sys.argv)
