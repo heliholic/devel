3b2832c6a7db38cf2ff3fc7ec03cb4a8ec482302
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-05-21 10:46:41 +0100

    Add swashplate wiggle indications

---
+++ src/main/flight/wiggle.h
@@ -0,0 +1,36 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "platform.h"
+
+enum {
+    WIGGLE_OFF = 0,
+    WIGGLE_ARMED,
+    WIGGLE_ERROR,
+    WIGGLE_BUZZER,
+};
+
+
+bool wiggleActive(void);
+void wiggleTrigger(uint8_t action);
+float wiggleGetAxis(int axis);
+
+void wiggleUpdate(timeUs_t wiggleUpdate);
+void wiggleInit(void);
+
