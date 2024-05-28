5836cc2445fdcfe04c6d2a5dbf91b5447d69628c
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-05-28 14:33:33 +0100

    Move telemetry config to pg/telemetry.h

---
+++ src/main/pg/telemetry.c
@@ -0,0 +1,82 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "platform.h"
+
+#ifdef USE_TELEMETRY
+
+#include "common/utils.h"
+#include "common/unit.h"
+
+#include "config/config.h"
+
+#include "pg/pg.h"
+#include "pg/pg_ids.h"
+#include "pg/rx.h"
+
+#include "drivers/timer.h"
+#include "drivers/serial.h"
+#include "drivers/serial_softserial.h"
+
+#include "io/serial.h"
+
+#include "fc/rc_modes.h"
+#include "fc/runtime_config.h"
+
+#include "rx/rx.h"
+
+#include "telemetry/telemetry.h"
+#include "telemetry/frsky_hub.h"
+#include "telemetry/hott.h"
+#include "telemetry/smartport.h"
+#include "telemetry/ltm.h"
+#include "telemetry/jetiexbus.h"
+#include "telemetry/mavlink.h"
+#include "telemetry/crsf.h"
+#include "telemetry/ghst.h"
+#include "telemetry/srxl.h"
+#include "telemetry/ibus.h"
+#include "telemetry/msp_shared.h"
+
+
+PG_REGISTER_WITH_RESET_TEMPLATE(telemetryConfig_t, telemetryConfig, PG_TELEMETRY_CONFIG, 0);
+
+PG_RESET_TEMPLATE(telemetryConfig_t, telemetryConfig,
+    .telemetry_inverted = false,
+    .halfDuplex = 1,
+    .gpsNoFixLatitude = 0,
+    .gpsNoFixLongitude = 0,
+    .frsky_coordinate_format = FRSKY_FORMAT_DMS,
+    .frsky_unit = UNIT_METRIC,
+    .frsky_vfas_precision = 0,
+    .hottAlarmSoundInterval = 5,
+    .pidValuesAsTelemetry = 0,
+    .report_cell_voltage = false,
+    .flysky_sensors = { 0, },
+    .telemetry_sensors = { 0, },
+    .mavlink_mah_as_heading_divisor = 0,
+    .crsf_flight_mode_reuse = CRSF_FM_REUSE_NONE,
+    .crsf_att_pitch_reuse = CRSF_ATT_REUSE_NONE,
+    .crsf_att_roll_reuse = CRSF_ATT_REUSE_NONE,
+    .crsf_att_yaw_reuse = CRSF_ATT_REUSE_NONE,
+);
+
+#endif
---
+++ src/main/pg/telemetry.h
@@ -0,0 +1,213 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "common/unit.h"
+
+#include "telemetry/ibus_shared.h"
+
+#include "pg/pg.h"
+
+
+typedef enum {
+    FRSKY_FORMAT_DMS = 0,
+    FRSKY_FORMAT_NMEA
+} frskyGpsCoordFormat_e;
+
+typedef enum
+{
+    TELEM_NONE              = 0,
+
+    TELEM_HEADSPEED,
+    TELEM_TAILSPEED,
+
+    TELEM_BATTERY_GROUP,
+    TELEM_BATTERY_VOLTAGE,
+    TELEM_BATTERY_CURRENT,
+    TELEM_BATTERY_CONSUMPTION,
+    TELEM_BATTERY_CHARGE_LEVEL,
+
+    TELEM_CELLS_GROUP,
+    TELEM_CELL1_VOLTAGE,
+    TELEM_CELL2_VOLTAGE,
+    TELEM_CELL3_VOLTAGE,
+    TELEM_CELL4_VOLTAGE,
+    TELEM_CELL5_VOLTAGE,
+    TELEM_CELL6_VOLTAGE,
+    TELEM_CELL7_VOLTAGE,
+    TELEM_CELL8_VOLTAGE,
+    TELEM_CELL9_VOLTAGE,
+    TELEM_CELL10_VOLTAGE,
+    TELEM_CELL11_VOLTAGE,
+    TELEM_CELL12_VOLTAGE,
+    TELEM_CELL13_VOLTAGE,
+    TELEM_CELL14_VOLTAGE,
+    TELEM_CELL15_VOLTAGE,
+    TELEM_CELL16_VOLTAGE,
+
+    TELEM_CELLS_MIN,
+    TELEM_CELLS_MAX,
+
+    TELEM_BEC_VOLTAGE,
+    TELEM_BEC_CURRENT,
+
+    TELEM_BUS_VOLTAGE,
+    TELEM_BUS_CURRENT,
+
+    TELEM_MCU_VOLTAGE,
+    TELEM_MCU_CURRENT,
+
+    TELEM_BUFFER_VOLTAGE,
+    TELEM_BUFFER_CURRENT,
+
+    TELEM_AIR_TEMPERATURE,
+    TELEM_ESC_TEMPERATURE,
+    TELEM_BEC_TEMPERATURE,
+    TELEM_MCU_TEMPERATURE,
+    TELEM_MOTOR_TEMPERATURE,
+    TELEM_BATTERY_TEMPERATURE,
+
+    TELEM_ALTITUDE,
+    TELEM_VARIOMETER,
+
+    TELEM_ACCEL_GROUP,
+    TELEM_ACCEL_X,
+    TELEM_ACCEL_Y,
+    TELEM_ACCEL_Z,
+
+    TELEM_ATTITUDE_GROUP,
+    TELEM_ATTITUDE_PITCH,
+    TELEM_ATTITUDE_ROLL,
+    TELEM_ATTITUDE_YAW,
+
+    TELEM_GPS_GROUP,
+    TELEM_GPS_HEADING,
+    TELEM_GPS_LATITUDE,
+    TELEM_GPS_LONGITUDE,
+    TELEM_GPS_ALTITUDE,
+    TELEM_GPS_DISTANCE,
+    TELEM_GPS_GROUNDSPEED,
+    TELEM_GPS_SAT_COUNT,
+    TELEM_GPS_DATE_TIME,
+
+    TELEM_AIRSPEED,
+    TELEM_RANGER_ALT,
+
+    TELEM_ESC1_GROUP,
+    TELEM_ESC1_VOLTAGE,
+    TELEM_ESC1_CURRENT,
+    TELEM_ESC1_ERPM,
+    TELEM_ESC1_POWER,
+    TELEM_ESC1_THROTTLE,
+    TELEM_ESC1_TEMP1,
+    TELEM_ESC1_TEMP2,
+    TELEM_ESC1_BEC_VOLTAGE,
+    TELEM_ESC1_BEC_CURRENT,
+    TELEM_ESC1_STATUS,
+
+    TELEM_ESC2_GROUP,
+    TELEM_ESC2_VOLTAGE,
+    TELEM_ESC2_CURRENT,
+    TELEM_ESC2_ERPM,
+    TELEM_ESC2_POWER,
+    TELEM_ESC2_THROTTLE,
+    TELEM_ESC2_TEMP1,
+    TELEM_ESC2_TEMP2,
+    TELEM_ESC2_BEC_VOLTAGE,
+    TELEM_ESC2_BEC_CURRENT,
+    TELEM_ESC2_STATUS,
+
+    TELEM_FC_CPU_LOAD,
+    TELEM_FC_SYS_LOAD,
+    TELEM_FC_RT_LOAD,
+    TELEM_FC_UPTIME,
+
+    TELEM_FLIGHT_MODE,
+    TELEM_ARMING_FLAGS,
+    TELEM_GOVERNOR_STATE,
+
+    TELEM_ADJFUNC_GROUP,
+    TELEM_ADJFUNC_ID,
+    TELEM_ADJFUNC_VALUE,
+
+    TELEM_PID_PROFILE,
+    TELEM_RATES_PROFILE,
+    TELEM_BATTERY_PROFILE,
+    TELEM_LED_PROFILE,
+
+    TELEM_ADC1,
+    TELEM_ADC2,
+    TELEM_ADC3,
+    TELEM_ADC4,
+
+    /* Compatibility values */
+    SENSOR_VOLTAGE         = TELEM_BATTERY_VOLTAGE,
+    SENSOR_CURRENT         = TELEM_BATTERY_CURRENT,
+    SENSOR_FUEL            = TELEM_BATTERY_CHARGE_LEVEL,
+    SENSOR_MODE            = TELEM_FLIGHT_MODE,
+    SENSOR_ACC_X           = TELEM_ACCEL_X,
+    SENSOR_ACC_Y           = TELEM_ACCEL_Y,
+    SENSOR_ACC_Z           = TELEM_ACCEL_Z,
+    SENSOR_PITCH           = TELEM_ATTITUDE_PITCH,
+    SENSOR_ROLL            = TELEM_ATTITUDE_ROLL,
+    SENSOR_HEADING         = TELEM_ATTITUDE_YAW,
+    SENSOR_ALTITUDE        = TELEM_ALTITUDE,
+    SENSOR_VARIO           = TELEM_VARIOMETER,
+    SENSOR_LAT_LONG        = TELEM_GPS_LATITUDE,
+    SENSOR_GROUND_SPEED    = TELEM_GPS_GROUNDSPEED,
+    SENSOR_DISTANCE        = TELEM_GPS_DISTANCE,
+    SENSOR_TEMPERATURE     = TELEM_ESC_TEMPERATURE,
+    SENSOR_CAP_USED        = TELEM_BATTERY_CONSUMPTION,
+    SENSOR_ADJUSTMENT      = TELEM_ADJFUNC_GROUP,
+    SENSOR_GOV_MODE        = TELEM_GOVERNOR_STATE,
+
+    ESC_SENSOR_CURRENT     = TELEM_ESC1_CURRENT,
+    ESC_SENSOR_VOLTAGE     = TELEM_ESC1_VOLTAGE,
+    ESC_SENSOR_RPM         = TELEM_ESC1_ERPM,
+    ESC_SENSOR_TEMPERATURE = TELEM_ESC1_TEMP1,
+
+} sensor_e;
+
+#define TELEM_SENSOR_COUNT 40
+
+typedef struct telemetryConfig_s {
+    int16_t gpsNoFixLatitude;
+    int16_t gpsNoFixLongitude;
+    uint8_t telemetry_inverted;
+    uint8_t halfDuplex;
+    uint8_t frsky_coordinate_format;
+    uint8_t frsky_unit;
+    uint8_t frsky_vfas_precision;
+    uint8_t hottAlarmSoundInterval;
+    uint8_t pidValuesAsTelemetry;
+    uint8_t report_cell_voltage;
+    uint8_t flysky_sensors[IBUS_SENSOR_COUNT];
+    uint16_t mavlink_mah_as_heading_divisor;
+    uint8_t crsf_flight_mode_reuse;
+    uint8_t crsf_att_pitch_reuse;
+    uint8_t crsf_att_roll_reuse;
+    uint8_t crsf_att_yaw_reuse;
+    uint8_t crsf_gps_heading_reuse;
+    uint8_t crsf_gps_ground_speed_reuse;
+    uint8_t crsf_gps_altitude_reuse;
+    uint8_t crsf_gps_sats_reuse;
+    uint16_t telemetry_sensors[TELEM_SENSOR_COUNT];
+} telemetryConfig_t;
+
+PG_DECLARE(telemetryConfig_t, telemetryConfig);
+
