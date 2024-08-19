d9adccd1e9f62c200b38e66cfd4c74813bad78cd
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-08-19 11:40:49 +0100

    Add tail_exponent

--- src/main/pg/mixer.h
+++ src/main/pg/mixer.h
@@ -94,6 +94,7 @@ typedef struct
     uint8_t   tail_rotor_mode;      // Tail motor vs. variable pitch tail
     uint8_t   tail_motor_idle;      // Idle throttle for tail motor
     int16_t   tail_center_trim;     // Tail center position offset
+    uint8_t   tail_exponent;        // Tail exponent
 
     uint8_t   swash_type;           // Swashplate type
     uint8_t   swash_ring;           // Swash ring size
