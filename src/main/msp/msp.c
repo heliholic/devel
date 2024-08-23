80f538fac2de133a087bd84fed2d9a77dbd13378
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-08-23 13:13:11 +0100

    🚁 Remove THROTTLE_BOOST

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -1963,11 +1963,7 @@ case MSP_NAME:
 #else
         sbufWriteU8(dst, 0);
 #endif
-#if defined(USE_THROTTLE_BOOST)
-        sbufWriteU8(dst, currentPidProfile->throttle_boost);
-#else
         sbufWriteU8(dst, 0);
-#endif
 #if defined(USE_ACRO_TRAINER)
         sbufWriteU8(dst, currentPidProfile->acro_trainer_angle_limit);
 #else
@@ -3196,11 +3192,7 @@ static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t cmdMSP, 
 #else
             sbufReadU8(src);
 #endif
-#if defined(USE_THROTTLE_BOOST)
-            currentPidProfile->throttle_boost = sbufReadU8(src);
-#else
             sbufReadU8(src);
-#endif
 #if defined(USE_ACRO_TRAINER)
             currentPidProfile->acro_trainer_angle_limit = sbufReadU8(src);
 #else
