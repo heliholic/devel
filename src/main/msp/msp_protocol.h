2b08297e3ec85e052943124a46bba7c3483a06f2
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-12-31 11:50:30 +0000

    Use 13.0 version

--- src/main/msp/msp_protocol.h
+++ src/main/msp/msp_protocol.h
@@ -55,7 +55,7 @@
 #define MSP_PROTOCOL_VERSION                 0
 
 #define API_VERSION_MAJOR                    13
-#define API_VERSION_MINOR                    1
+#define API_VERSION_MINOR                    0
 #define API_VERSION_LENGTH                   2
 
 #define FLIGHT_CONTROLLER_IDENTIFIER_LENGTH  4
