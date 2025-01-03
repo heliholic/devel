55f7ab55ff2562797ae061b00caad109cd5bdb4e
Petri Mattila <petri.mattila@unipart.com>
2025-01-03 12:20:21 +0000

    🚁 Remove pid

--- mk/source.mk
+++ mk/source.mk
@@ -150,7 +150,6 @@ COMMON_SRC = \
             flight/imu.c \
             flight/mixer.c \
             flight/pid.c \
-            flight/pid_init.c \
             flight/position.c \
             flight/pos_hold.c \
             flight/servos.c \
@@ -444,7 +443,6 @@ SPEED_OPTIMISED_SRC += \
 SIZE_OPTIMISED_SRC += \
             sensors/gyro_init.c \
             sensors/acceleration_init.c \
-            flight/pid_init.c \
             cli/cli.c \
             cli/settings.c \
             drivers/light_ws2811strip.c \
