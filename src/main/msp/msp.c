aa2657270c92fbc18236f556f8cfd67c4ee816e2
Petri Mattila <petri.t.j.mattila@gmail.com>
2025-05-25 22:35:56 +0100

    WIP

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -2004,7 +2004,7 @@ static bool mspProcessOutCommand(int16_t cmdMSP, sbuf_t *dst)
         sbufWriteU16(dst, governorConfig()->gov_tracking_time);
         sbufWriteU16(dst, governorConfig()->gov_recovery_time);
         sbufWriteU16(dst, governorConfig()->gov_low_throttle_timeout);
-        sbufWriteU16(dst, governorConfig()->gov_lost_headspeed_timeout);
+        sbufWriteU16(dst, 0); // governorConfig()->gov_lost_headspeed_timeout
         sbufWriteU16(dst, governorConfig()->gov_autorotation_timeout);
         sbufWriteU16(dst, 0); // governorConfig()->gov_autorotation_bailout_time
         sbufWriteU16(dst, governorConfig()->gov_autorotation_min_entry_time);
@@ -3463,7 +3463,7 @@ static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t cmdMSP, 
         governorConfigMutable()->gov_tracking_time = sbufReadU16(src);
         governorConfigMutable()->gov_recovery_time = sbufReadU16(src);
         governorConfigMutable()->gov_low_throttle_timeout = sbufReadU16(src);
-        governorConfigMutable()->gov_lost_headspeed_timeout = sbufReadU16(src);
+        sbufReadU16(src);; // governorConfigMutable()->gov_lost_headspeed_timeout = sbufReadU16(src);
         governorConfigMutable()->gov_autorotation_timeout = sbufReadU16(src);
         sbufReadU16(src); // governorConfigMutable()->gov_autorotation_bailout_time = sbufReadU16(src);
         governorConfigMutable()->gov_autorotation_min_entry_time = sbufReadU16(src);
--- src/main/pg/governor.h
+++ src/main/pg/governor.h
@@ -29,7 +29,6 @@ typedef struct governorConfig_s {
     uint16_t gov_tracking_time;
     uint16_t gov_recovery_time;
     uint16_t gov_low_throttle_timeout;
-    uint16_t gov_lost_headspeed_timeout;
     uint16_t gov_autorotation_timeout;
     uint16_t gov_autorotation_min_entry_time;
     uint8_t  gov_handover_throttle;
