855e2b032d24e3abc470316af682e9173bc0ead3
Petri Mattila <petri.t.j.mattila@gmail.com>
2025-08-11 10:24:10 +0100

    Refactor throttle channel range and arm limit

--- src/main/rx/rx.c
+++ src/main/rx/rx.c
@@ -301,7 +301,7 @@ void rxInit(void)
         validRxSignalTimeout[i] = millis() + MAX_INVALID_PULSE_TIME_MS;
     }
 
-    rcInput[THROTTLE] = rcControlsConfig()->rc_arm_throttle - 10;
+    rcInput[THROTTLE] = 975;
 
     // Initialize ARM switch to OFF position when arming via switch is defined
     // TODO - move to rc_mode.c
