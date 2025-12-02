ad153693803b23af2ad7973c9b15e063c97d0305
Petri Mattila <petri.t.j.mattila@gmail.com>
2025-12-02 03:52:50 +0000

    Add IIM4265x driver

--- src/main/sensors/acceleration_init.c
+++ src/main/sensors/acceleration_init.c
@@ -237,9 +237,11 @@ retry:
         FALLTHROUGH;
 #endif
 
-#if defined(USE_ACC_SPI_ICM42605) || defined(USE_ACC_SPI_ICM42688P)
+#if defined(USE_ACC_SPI_ICM42605) || defined(USE_ACC_SPI_ICM42688P) || defined(USE_ACCGYRO_IIM42652) || defined(USE_ACCGYRO_IIM42653)
     case ACC_ICM42605:
     case ACC_ICM42688P:
+    case ACC_IIM42652:
+    case ACC_IIM42653:
         if (icm426xxSpiAccDetect(dev)) {
             switch (dev->mpuDetectionResult.sensor) {
             case ICM_42605_SPI:
@@ -247,6 +249,12 @@ retry:
                 break;
             case ICM_42688P_SPI:
                 accHardware = ACC_ICM42688P;
+                break;
+            case IIM_42652_SPI:
+                accHardware = ACC_IIM42652;
+                break;
+            case IIM_42653_SPI:
+                accHardware = ACC_IIM42653;
                 break;
             default:
                 accHardware = ACC_NONE;
